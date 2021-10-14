3e7c723f6fc0932bc27c24236543665203236568
ChipTheKing <chiplyons1@att.net>
2021-10-14 13:45:37 -0400

    added class files and updated scanner to read lines

--- Scanner/Scanner.py
+++ Scanner/Scanner.py
@@ -1,1 +1,15 @@
-print("Hello World")
+def read_lines(file_name):
+    file = open(file_name, 'r')
+    Lines = file.readlines()
+
+    count = 0
+    for line in Lines:
+        count += 1
+        print("Line{}: {}".format(count, line.strip()))
+
+
+def main():
+    read_lines('/4308-Project-Fall-2021/Julia-Files/Test1.jl')
+
+
+main()
